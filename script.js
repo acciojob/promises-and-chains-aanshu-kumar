8112ab2388d45b685d8fba4d8dbbbb19650d7268
Anshu kumar <40786793+aanshu-kumar@users.noreply.github.com>
2024-12-24 20:12:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+let name=document.getElementById("name");
+let age = document.getElementById("age");
+const mypromise = new Promise((resolve,reject)=>{
+	
+	setTimeout(()=>{
+		if(age>=18)
+		resolve(`Welcome ${name}. You can vote.`)
+	else
+		reject(`Oh sorry ${name}. You aren't old enough.`)
+	},4000)
+})
+
+mypromise.then((response)=>{
+	alert(response);
+}).catch((err)=>{
+	alert(err);
+})
