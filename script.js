aa49711052142302eee01c35455c983212a9246d
Anshu kumar <40786793+aanshu-kumar@users.noreply.github.com>
2024-12-25 15:10:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,21 +1,20 @@
 //your JS code here. If required.
-function verify(){
-	
-let name=document.getElementById("name").value;
-let age = document.getElementById("age").value;
-const mypromise = new Promise((resolve,reject)=>{
-	
-	setTimeout(()=>{
-		if(age>=18)
-		resolve(`Welcome ${name}. You can vote.`)
-	else
-		reject(`Oh sorry ${name}. You aren't old enough.`)
-	},4000)
-})
 
-mypromise.then((response)=>{
-	alert(response);
-}).catch((err)=>{
-	alert(err);
-})
-}
+document.getElementById("submit").addEventListener("click", () => {
+  let name = document.getElementById("name").value;
+  let age = document.getElementById("age").value;
+  const mypromise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (age >= 18) resolve(`Welcome ${name}. You can vote.`);
+      else reject(`Oh sorry ${name}. You aren't old enough.`);
+    }, 4000);
+  });
+
+  mypromise
+    .then((response) => {
+      alert(response);
+    })
+    .catch((err) => {
+      alert(err);
+    });
+});
