5d686cd0a49e67e10aea1fcf335d15ed244b5e3e
Anshu kumar <40786793+aanshu-kumar@users.noreply.github.com>
2024-12-28 18:39:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-document.getElementById("myForm").addEventListener("submit", (event) => {
-  event.preventDefault(); // prevent the form from submitting normally
-
+document.getElementById("btn").addEventListner("click",()=>{
   let name = document.getElementById("name").value;
   let age = document.getElementById("age").value;
 
   const mypromise = new Promise((resolve, reject) => {
     setTimeout(() => {
-      if (age >= 18) resolve(`Welcome ${name}. You can vote.`);
-      else reject(`Oh sorry ${name}. You aren't old enough.`);
+      if (age >= 18) resolve(`Welcome, ${name}. You can vote.`);
+      else if reject(`Oh sorry ${name}. You aren't old enough.`);
     }, 4000);
+	  else
+	  reject(`Please enter valid details`);
   });
 
   mypromise
